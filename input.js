6dc6a38c3e6b6c22cbc28d115ad9991da93b0267
prashanthk02 <prashanth.k369@gmail.com>
2022-03-16 18:22:04 -0400

    updated

--- input.js
+++ input.js
@@ -1,20 +1,35 @@
+let connection;// Stores the active TCP connection object.
+//const net = require("net");
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
   const handleUserInput = (data) => {
     if (data === '\u0003') {
       process.exit();
     }
+    if (data === 'w') {
+      connection.write('Move: up');
+    }
+    if (data === 'a') {
+      connection.write('Move: left');
+    }
+    if (data === 's') {
+      connection.write('Move: down');
+    }
+    if (data === 'd') {
+      connection.write('Move: right');
+    }
 
   };
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
 module.exports = { setupInput };
